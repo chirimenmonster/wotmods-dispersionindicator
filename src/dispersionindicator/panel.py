5ebcca79874b11055f090e9cba4f251607b85935
chirimenmonster <chirimenmonster@users.noreply.github.com>
2019-03-12 11:59:05 +0900

    wip

--- src/dispersionindicator/panel.py
+++ src/dispersionindicator/panel.py
@@ -1,134 +1,122 @@
 
 import GUI
-import ResMgr
-from gui import g_guiResetters
 
 
-BGIMAGE_FILE = '${resource_dir}/bgimage.dds'
-DEFAULT_WIDTH = 280
-DEFAULT_HEIGHT = 16
+class Widget(object):
+    @property
+    def visible(self):
+        return self.widget.visible
 
-class Config:
-    colour = (255, 255, 0)
-    alpha = 127
-    font = 'default_small.font'
-    padding_top = 4
-    padding_bottom = 4
-    panel_width = 280
-    panel_offset = (-200, 50)
-    bgimage = BGIMAGE_FILE
+    @visible.setter
+    def visible(self, visible):
+        self.widget.visible = visible
+    
+    @property
+    def width(self):
+        return self.widget.width
 
-g_config = Config()
+    @width.setter
+    def width(self, width):
+        self.widget.width = width
+
+    @property
+    def height(self):
+        return self.widget.width
+
+    @height.setter
+    def height(self, height):
+        self.widget.height = height
+
+    @property
+    def horizontalAnchor(self):
+        return self.widget.horizontalAnchor
+
+    @horizontalAnchor.setter
+    def horizontalAnchor(self, horizontanAnchor):
+        self.widget.horizontalAnchor = horizontalAnchor
+
+    @property
+    def verticalAnchor(self):
+        return self.widget.verticalAnchor
+
+    @verticalAnchor.setter
+    def verticalAnchor(self, verticalAnchor):
+        self.widget.verticalAnchor = verticalAnchor
+
+    @property
+    def position(self):
+        return self.widget.position
+
+    @position.setter
+    def position(self, position):
+        self.widget.position = position
 
 
-def test():
-    class LabelConfig: pass
-    config = LabelConfig()
-    config.title = 'Current Time'
-    config.unit = 's'
-    config.getStatus = lambda: BigWorld.time()
-    Panel([config])
-
-
-class Label(object):
-    def __init__(self, horizontalAnchor='LEFT', verticalAnchor='TOP', text='', func=None, x=0, y=0):
+class LabelWidget(Widget):
+    def __init__(self, text=''):
         self.widget = GUI.Text(text)
         self.widget.horizontalPositionMode = 'PIXEL'
         self.widget.verticalPositionMode = 'PIXEL'
         self.widget.colourFormatting = True
-        self.widget.font = g_config.font
-        self.widget.colour = g_config.colour + (g_config.alpha, )
-        self.widget.horizontalAnchor = horizontalAnchor
-        self.widget.verticalAnchor = verticalAnchor
-        self.widget.position = (x, y, 1)
-        self.widget.visible = True
-        self.__func = func
+        self.horizontalAnchor = 'LEFT'
+        self.verticalAnchor = 'TOP'
 
-    def onUpdate(self):
+    def update(self):
         if callable(self.__func):
-            self.widget.text = str(self.__func())
+            self.text = str(self.__func())
+
+    def setCallback(self, callback):
+        self.__func = callback
+
+    @property
+    def text(self):
+        return self.widget.text
+
+    @text.setter
+    def text(self, text):
+        self.widget.text = text
+
+    @property
+    def font(self):
+        return self.widget.font
+
+    @font.setter
+    def font(self, font):
+        self.widget.font = font
+
+    @property
+    def colour(self):
+        return self.widget.colour
+
+    @colour.setter
+    def colour(self, colour):
+        self.widget.colour = colour
 
 
-class PanelLine(object):
-    def __init__(self, config, width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT, x=0, y=0):
+class PanelWidget(Widget):
+    def __init__(self, bgimage=None):
         self.children = []
-        self.widget = GUI.Window(g_config.bgimage)
+        self.widget = GUI.Window(bgimage)
         self.widget.materialFX = 'BLEND'
-        self.widget.horizontalAnchor = 'LEFT'
-        self.widget.verticalAnchor = 'TOP'
         self.widget.horizontalPositionMode = 'PIXEL'
         self.widget.verticalPositionMode = 'PIXEL'
         self.widget.widthMode = 'PIXEL'
         self.widget.heightMode = 'PIXEL'
-        self.widget.width = width
-        self.widget.height = height
-        self.widget.position = (x, y, 1)
-        self.addChild(Label(text=config.title, horizontalAnchor='RIGHT', x=width-128, y=0))
-        self.addChild(Label(func=config.func, horizontalAnchor='RIGHT', x=width-72, y=0))
-        self.addChild(Label(text=config.unit, horizontalAnchor='LEFT', x=width-60, y=0))
-        self.widget.visible = True
+        self.horizontalAnchor = 'LEFT'
+        self.verticalAnchor = 'TOP'
 
     def addChild(self, child):
         self.children.append(child)
         self.widget.addChild(child.widget)
 
-    def onUpdate(self):
+    def update(self):
         for child in self.children:
-            if callable(getattr(child, 'onUpdate')):
-                child.onUpdate()
+            if callable(getattr(child, 'update')):
+                child.update()
 
-
-class Panel(object):
-    def __init__(self, items=[]):
-        self.children = []
-        print g_config.bgimage, ResMgr.isFile(g_config.bgimage)
-        self.widget = GUI.Window(g_config.bgimage)
-        self.widget.materialFX = 'BLEND'
-        self.widget.horizontalAnchor = 'RIGHT'
-        self.widget.verticalAnchor = 'CENTER'
-        self.widget.horizontalPositionMode = 'PIXEL'
-        self.widget.verticalPositionMode = 'PIXEL'
-        self.widget.widthMode = 'PIXEL'
-        self.widget.heightMode = 'PIXEL'
-        self.widget.width = g_config.panel_width
-        y = g_config.padding_top
-        for item in items:
-            child = PanelLine(item, x=0, y=y)
-            self.addChild(child)
-            #y = y + child.widget.height
-            y = y + DEFAULT_HEIGHT
-        self.widget.height = y + g_config.padding_bottom
-        self.widget.visible = False
-        self.addChild(Label(text='TEST', horizontalAnchor='RIGHT', x=128, y=10))
-
-    def addChild(self, child):
-        self.children.append(child)
-        self.widget.addChild(child.widget)
-
-    def start(self):
-        print 'panel.start'
+    def addRoot(self):
         GUI.addRoot(self.widget)
-        g_guiResetters.add(self.onScreenResolutionChanged)
-        self.onScreenResolutionChanged()
-        self.widget.visible = True
 
-    def stop(self):
-        print 'panel.stop'
-        g_guiResetters.discard(self.onScreenResolutionChanged)
+    def delRoot(self):
         GUI.delRoot(self.widget)
 
-    def onUpdate(self):
-        for child in self.children:
-            if callable(getattr(child, 'onUpdate')):
-                child.onUpdate()
-
-    def onScreenResolutionChanged(self):
-        screen = GUI.screenResolution()
-        center = ( screen[0] / 2, screen[1] / 2)
-        x = center[0] + g_config.panel_offset[0]
-        y = center[1] + g_config.panel_offset[1]
-        self.widget.position = (x, y, 1)
-        print self.widget.position, self.widget.width, self.widget.height
-
-    def toggleVisible(self):
-        self.widget.visible = not self.widget.visible
--- src/dispersionindicator/status.py
+++ src/dispersionindicator/status.py
@@ -1,12 +1,31 @@
 
 import math
 import BigWorld
+from gui import g_guiResetters
+
 from panel import Panel
 
+
+BGIMAGE_FILE = '${resource_dir}/bgimage.dds'
+DEFAULT_WIDTH = 280
+DEFAULT_HEIGHT = 16
 
 CONSTANT = {
     'MS_TO_KMH':    3600.0 / 1000.0
 }
+
+class Config:
+    colour = (255, 255, 0)
+    alpha = 127
+    font = 'default_small.font'
+    padding_top = 4
+    padding_bottom = 4
+    panel_width = 280
+    panel_offset = (-200, 50)
+    line_height = 16
+    bgimage = BGIMAGE_FILE
+
+g_config = Config()
 
 
 def init():
@@ -27,33 +46,8 @@ def shotResultIndicatorPlugin_stop(orig, self, *args, **kwargs):
 
 def shotResultIndicatorPlugin_onGunMarkerStateChanged(orig, self, *args, **kwargs):
     result = orig(self, *args, **kwargs)
-    g_panel.onUpdate()
+    g_panel.update()
     return result
-
-
-class IndicatorPanel(Panel):
-    def __init__(self):
-        configList = self.__genSettings()
-        print configList
-        super(IndicatorPanel, self).__init__(configList)
-
-    def __genSettings(self):
-        class Config: pass
-        configList = []
-        print g_config
-        for setting in g_config['panelItems']:
-            name = setting['status']
-            factor = setting['factor']
-            template = setting['format']
-            if isinstance(factor, str):
-                factor = CONSTANT.get(factor, 1.0)
-            config = Config()
-            config.title = setting['title']
-            config.func = lambda: template.format(getattr(g_status, name) * factor)
-            config.unit = setting['unit']
-            configList.append(config)
-            print name
-        return configList
 
 def playerAvatar_getOwnVehicleShotDispersionAngle(orig, self, turretRotationSpeed, withShot = 0):
     result = orig(self, turretRotationSpeed, withShot)
@@ -64,6 +58,107 @@ def playerAvatar_getOwnVehicleShotDispersionAngle(orig, self, turretRotationSpee
     g_status.withShot = withShot
     g_status._getOwnVehicleShotDispersionAngle(avatar)
     return result
+
+
+class IndicatorPanel(object):
+    def __init__(self):
+        self.label_font = g_config.font
+        self.label_colour = g_config.colour + (g_config.alpha, )
+        self.line_height = g_config.line_height
+        self.padding_top = g_config.padding_top
+        self.padding_bottom = g_config.padding_bottom
+        self.panel_offset = g_config.panel_offset
+        self.panel_width = g_config.panel_width
+        self.bgimage = g_config.bgimage
+        self.panel = createWidgetTree()
+
+    def start(self):
+        print 'panel.start'
+        self.panel.addRoot()
+        g_guiResetters.add(self.onScreenResolutionChanged)
+        self.updatePosition()
+        self.panel.visible = True
+        
+    def stop(self):
+        print 'panel.stop'
+        g_guiResetters.discard(self.onScreenResolutionChanged)
+        self.panel.visible = False
+        self.panel.delRoot()
+
+    def toggleVisible(self):
+        self.panel.visible = not self.panel.visible
+
+    def onGunMarkerStateChanged(self):
+        self.panel.update()
+
+    def onScreenResolutionChanged(self):
+        self.updatePosition()
+
+    def updatePosition(self):
+        screen = GUI.screenResolution()
+        center = ( screen[0] / 2, screen[1] / 2)
+        x = center[0] + self.panel_offset[0]
+        y = center[1] + self.panel_offset[1]
+        self.panel.position = (x, y, 1)
+        print self.panel.position, self.panel.width, self.panel.height
+
+    def createWidgetTree(self):
+        panel = PanelWidget(self.bgimage)
+        y = self.padding_top
+        for setting in g_config['panelItems']:
+            child = self.createPanelLine(setting)
+            panel.addChild(child)
+            child.position = (0, y, 1)
+            y = y + child.height
+        panel.width = self.panel_width
+        panel.height = y + self.padding_bottom
+        panel.horizontalAnchor = 'RIGHT'
+        panel.verticalAnchor = 'CENTER'
+        return panel
+
+    def createPanelLine(self, setting):
+        name = setting['status']
+        factor = setting['factor']
+        template = setting['format']
+        if isinstance(factor, str):
+            factor = CONSTANT.get(factor, 1.0)
+        argList = [
+            {
+                'text':     setting['title'],
+                'align':    'RIGHT',
+                'x':        width - 128
+            },
+            {
+                'func':     lambda: template.format(getattr(g_status, name) * factor),
+                'align':    'RIGHT',
+                'x':        width - 72    
+            },
+            {
+                'text':     setting['unit'],
+                'x':        width - 60
+            }
+        ]
+        panel = PanelWidget(self.bgimage)
+        for kwarg in argList:
+            label = LabelWidget(**kwarg)
+            panel.addChild(label)
+        panel.width = self.panel_width
+        panel.height = self.line_height
+        panel.visible = True
+        return panel
+
+    def createLabel(self, text=None, func=None, align='LEFT', x=0):
+        label = LabelWidget()
+        if text is not None:
+            label.text = text
+        if func is not None:
+            label.setCallback(func)
+        label.font = self.label_font
+        label.colour = self.label_colour
+        label.horizontalAnchor = align
+        label.position = (x, 0, 1)
+        label.visible = True
+        return label
 
 
 class Status(object):
